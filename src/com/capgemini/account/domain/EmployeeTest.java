64a8310a284f988beebf7f5ac124eed2dc2ff31c
ltejasri <ltejasri@IN-MUM-LDVM034.corp.capgemini.com>
2018-09-03 08:56:17 -0400

    last commit

---
+++ src/com/capgemini/account/domain/EmployeeTest.java
@@ -0,0 +1,30 @@
+package com.capgemini.account.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+	
+	Employee employee;
+	
+	@BeforeEach
+	void setUp() {
+		employee = new Employee(1234, "John Doe");
+	}
+	
+	@Test
+	void testSalaryCalculation() {
+		assertEquals(18600,employee.salaryCalculation(10000,5000));
+		//fail("Not yet implemented");
+	}
+	
+	@AfterEach
+	void tearDown() {
+		employee = null;
+	}
+	
+
+}
